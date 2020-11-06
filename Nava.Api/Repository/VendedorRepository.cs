bae1ac162446b37b1538d4e4c094db619b1dd989
Ricardo Pereira <ricardo.iviz@outlook.com.br>
2020-11-06 07:08:45 -0300

    ajustes

--- Nava.Api/Repository/VendedorRepository.cs
+++ Nava.Api/Repository/VendedorRepository.cs
@@ -7,15 +7,12 @@ namespace Nava.Api.Repository
     /// </summary>
     public class VendedorRepository : RepositoryBase<Vendedor>, IVendedorRepository
     {
-        private readonly DatabaseContext context;
-
         /// <summary>
         /// Construtor do Vendedor Repository
         /// </summary>
         public VendedorRepository(DatabaseContext context)
             : base(context, context.Vendedores)
         {
-            this.context = context;
         }
     }
 }
