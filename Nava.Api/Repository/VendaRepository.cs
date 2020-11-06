664c85ca2b60bd9b3d4e10b34786b22313e388f0
Ricardo Pereira <ricardo.iviz@outlook.com.br>
2020-11-06 07:06:21 -0300

    .

--- Nava.Api/Repository/VendaRepository.cs
+++ Nava.Api/Repository/VendaRepository.cs
@@ -3,7 +3,6 @@ using Nava.Api.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Nava.Api.Repository
